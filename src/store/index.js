6ec201f6811b461847214fbafe76a65586de1032
wql <3224177294@qq.com>
2023-03-21 19:02:40 +0800

    优化代码

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
+
 import piniaPluginPersist from 'pinia-plugin-persist';
 // 使用setup模式定义
 export const useStatusStore = defineStore("status", () => {
@@ -10,3 +11,17 @@ export const useStatusStore = defineStore("status", () => {
 
   return { status, choose }; 
 });
+export const useActiveStore = defineStore('active',()=>{
+  const isActive = ref([
+    'active', '', '', '', '', ''
+])
+const active = (index) => {
+    for (let i = 0; i < isActive.length; i++) {
+        isActive.value[i] = ''
+    }
+    isActive.value[index] = 'active'
+    console.log(isActive.value)
+}
+
+return {isActive,active}
+})
--- src/views/employee/EmployeeView.vue
+++ src/views/employee/EmployeeView.vue
@@ -1,3 +1,4 @@
+import { ref, reactive, onMounted } from 'vue';
 <template>
     <div class="w-full h-full overflow-hidden flex ">
         <aside class="fixed w-56 h-full">
@@ -5,18 +6,18 @@
                 <li class="menu-title">
                     <span>经理</span>
                 </li>
-                <li><a href="/employee/manageenroll">管理培训申请</a></li>
-                <li><a href="/employee/managetrain">查看培训情况</a></li>
-                <li><a href="/employee/managematerial">查看办公资料</a></li>
+                <li><a :class="isActive[0]" href="/employee/manageenroll" @click="active(0)">管理培训申请</a></li>
+                <li><a :class="isActive[1]" href="/employee/managetrain" @click="active(1)">查看培训情况</a></li>
+                <li><a :class="isActive[2]" href="/employee/managematerial" @click="active(2)">查看办公资料</a></li>
                 <li class="menu-title">
                     <span>执行人</span>
                 </li>
-                <li><a href="/employee/managecourse">管理课程</a></li>
-                <li><a href="/employee/manageteacher">管理讲师</a></li>
+                <li><a :class="isActive[3]" href="/employee/managecourse" @click="active(3)">管理课程</a></li>
+                <li><a :class="isActive[4]" href="/employee/manageteacher" @click="active(4)">管理讲师</a></li>
                 <li class="menu-title">
                     <span>现场工作人员</span>
                 </li>
-                <li><a href="/employee/manageattendance">签到</a></li>
+                <li><a :class="isActive[5]" href="/employee/manageattendance" @click="active(5)">签到</a></li>
             </ul>
         </aside>
         <main class='w-full ml-60 mr-4'>
@@ -24,3 +25,24 @@
         </main>
     </div>
 </template>
+<script setup>
+import { ref, reactive,onUpdated,onMounted} from 'vue'
+// import {useActiveStore} from "../../store"
+// import { storeToRefs } from 'pinia'
+// const store = useActiveStore()
+// const {isActive} = storeToRefs(store)
+// const {active} = store
+const isActive = ref([
+    'active', '', '', '', '', ''
+])
+const active = (index) => {
+    for (let i = 0; i < isActive.length; i++) {
+        isActive.value[i] = ''
+    }
+    isActive.value[index] = 'active'
+    console.log(isActive.value)
+}
+onMounted(() => {
+    console.log(isActive.value)
+})
+</script>

73c3feb04d2623f8b4a35833bf68740001d86405
YangZY <2021212038@stu.hit.edu.cn>
2023-04-13 17:14:55 +0800

    封装toast

---
+++ src/components/common/toast.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="toast toast-top toast-center z-50" v-if="msg!==undefined">
+    <div :class="'w-max max-w-xl alert alert-'+type">
+      <div>
+        <span class="break-all">{{ msg }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['type', 'msg'])
+</script>
